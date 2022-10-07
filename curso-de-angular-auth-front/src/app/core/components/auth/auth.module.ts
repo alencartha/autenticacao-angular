a23b2903c0722b01f53ca6ef74b05621ce1925be
alencartha <alencar_tha@hotmail.com>
2022-10-07 11:16:57 -0300

    metodo post autenticação e tratamento msg erro

--- curso-de-angular-auth-front/src/app/core/components/auth/auth.module.ts
+++ curso-de-angular-auth-front/src/app/core/components/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignComponent } from './pages/sign/sign.component';
@@ -16,7 +15,6 @@ import { SignComponent } from './pages/sign/sign.component';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
   ]
 })
 export class AuthModule { }
--- curso-de-angular-auth-front/src/app/core/components/auth/pages/sign/sign.component.ts
+++ curso-de-angular-auth-front/src/app/core/components/auth/pages/sign/sign.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+
+//service
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-sign',
@@ -9,18 +12,28 @@ import {FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class SignComponent implements OnInit {
 
   public formAuth: FormGroup = this.formBuilder.group({
-    email:['', [Validators.required, Validators.email]],
-    password:['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
   })
 
-  constructor(private formBuilder: FormBuilder) { }
+  public messageError: string = ''
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
-  public submitForm(){
-    if(this.formAuth.valid){
-      console.log(this.formAuth)
+  public submitForm() {
+    if (this.formAuth.valid) {
+      this.authService.sign(
+        {
+          email: this.formAuth.value.email,
+          password: this.formAuth.value.password
+        }
+      ).subscribe({
+        next: (res) => res,
+        error: (e) => this.messageError  = e.error.message
+      })
     }
   }
 
