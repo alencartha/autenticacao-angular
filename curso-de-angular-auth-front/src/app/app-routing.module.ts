64ccba56949dfbd280bc59e297d64e8365d9612c
alencartha <alencar_tha@hotmail.com>
2022-10-07 12:14:04 -0300

    finaliza autenticacao com protecao de rota

--- curso-de-angular-auth-front/src/app/app-routing.module.ts
+++ curso-de-angular-auth-front/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './core/guard/auth-guard.guard';
 
 const routes: Routes = [
   {
@@ -8,6 +9,7 @@ const routes: Routes = [
   },
   {
     path: 'admin',
+    canActivateChild: [AuthGuardGuard],
     loadChildren: () => import('../app/components/admin/admin.module').then(module => module.AdminModule)
   }
 ];
--- curso-de-angular-auth-front/src/app/core/services/auth.service.ts
+++ curso-de-angular-auth-front/src/app/core/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {Observable, map, catchError, throwError} from 'rxjs'
-import {HttpClient} from '@angular/common/http'
+import { Observable, map, catchError, throwError } from 'rxjs'
+import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt'
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +13,37 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  public sign(payload: {email:string, password: string}): Observable<any>{
-    return this.http.post<{token:string}>(`${this.url}/sign`, payload).pipe(
-      map((data) =>{
+  public sign(payload: { email: string, password: string }): Observable<any> {
+    return this.http.post<{ token: string }>(`${this.url}/sign`, payload).pipe(
+      map((data) => {
         localStorage.removeItem('access_token')
         localStorage.setItem('access_token', JSON.stringify(data.token))
         return this.router.navigate(['admin'])
       }),
-      catchError((err)=>{
+      catchError((err) => {
         console.log(err)
-        if(err.error.message)
-          return throwError(()=> err.error.message)
-          return throwError(()=> "No momento nao conseguimos validar od dados, tente novamente mais tarde")
+        if (err.error.message)
+          return throwError(() => err.error.message)
+        return throwError(() => "No momento nao conseguimos validar od dados, tente novamente mais tarde")
       })
     )
   }
 
-  public logout(){
+  public logout() {
     localStorage.removeItem('access_token')
     return this.router.navigate([''])
+  }
+
+  public isAutenticaded(): boolean {
+    const token = localStorage.getItem('access_token')
+
+    if (!token) {
+      return false
+    } else {
+      const jwtHelper = new JwtHelperService();
+      return !jwtHelper.isTokenExpired(token)
+    }
+
+
   }
 }
