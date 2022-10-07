a28f595616213548afa3014d22ae2784bb9a276a
alencartha <alencar_tha@hotmail.com>
2022-10-07 10:54:11 -0300

    implementa formulario reativo

--- curso-de-angular-auth-front/src/app/app.module.ts
+++ curso-de-angular-auth-front/src/app/app.module.ts
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- curso-de-angular-auth-front/src/app/core/components/auth/auth.module.ts
+++ curso-de-angular-auth-front/src/app/core/components/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignComponent } from './pages/sign/sign.component';
+
 
 
 @NgModule({
@@ -11,7 +14,9 @@ import { SignComponent } from './pages/sign/sign.component';
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AuthModule { }
